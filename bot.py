6badc374168f6c4d5d3cbf2c22148613a9088ad5
danieltan2018 <50669678+danieltan2018@users.noreply.github.com>
2020-01-03 19:14:58 +0800

    Update messages

--- bot.py
+++ bot.py
@@ -11,6 +11,7 @@ import time
 from params import bottoken, port
 import json
 from datetime import datetime
+import re
 
 # pip install pyopenssl
 from requests import get
@@ -65,42 +66,55 @@ def start(update, context):
     user_id = str(update.message.chat_id)
     if user_id.startswith('-'):
         message = context.bot.send_message(
-            chat_id=user_id, text='*Added to group successfully*', parse_mode=telegram.ParseMode.MARKDOWN)
+            chat_id=user_id, text='*I will send the prayer list to this group.*\n\nTo change group, /leave this group first.', parse_mode=telegram.ParseMode.MARKDOWN)
         global groups
         groups[user_id] = str(message.message_id)
         with open('groups.json', 'w') as groupfile:
             json.dump(groups, groupfile)
+        new(update, context)
     else:
         first_name = update.message.from_user.first_name
         last_name = update.message.from_user.last_name
         full_name = (str(first_name or '') + ' ' +
                      str(last_name or '')).strip()
         context.bot.send_message(
-            chat_id=user_id, text='Hi *{}*! You may leave at any time using /leave.'.format(full_name), parse_mode=telegram.ParseMode.MARKDOWN)
+            chat_id=user_id, text='Hi *{}*! You have been added to the prayer list.\n\nYou may leave at any time using /leave.'.format(full_name), parse_mode=telegram.ParseMode.MARKDOWN)
         global users
         users[user_id] = {'name': full_name, 'prayer': ''}
         with open('users.json', 'w') as userfile:
             json.dump(users, userfile)
+        groupedit(context)
 
 
 def leave(update, context):
-    user_id = str(update.effective_user.id)
-    global users
-    del users[user_id]
-    with open('users.json', 'w') as userfile:
-        json.dump(users, userfile)
-    context.bot.send_message(
-        chat_id=user_id, text='*Goodbye!*', parse_mode=telegram.ParseMode.MARKDOWN)
+    user_id = str(update.message.chat_id)
+    if user_id.startswith('-'):
+        global groups
+        del groups[user_id]
+        with open('groups.json', 'w') as groupfile:
+            json.dump(groups, groupfile)
+        context.bot.send_message(
+            chat_id=user_id, text='*I will stop sending the prayer list to this group.*', parse_mode=telegram.ParseMode.MARKDOWN)
+    else:
+        global users
+        del users[user_id]
+        with open('users.json', 'w') as userfile:
+            json.dump(users, userfile)
+        context.bot.send_message(
+            chat_id=user_id, text='*Goodbye!*', parse_mode=telegram.ParseMode.MARKDOWN)
+        groupedit(context)
 
 
 def new(update, context):
     global groups
     for group in groups:
         message = context.bot.send_message(
-            chat_id=int(group), text='*Prayer List*', parse_mode=telegram.ParseMode.MARKDOWN)
+            chat_id=int(group), text='<b>PRAYER LIST</b>', parse_mode=telegram.ParseMode.HTML)
         groups[group] = str(message.message_id)
         with open('groups.json', 'w') as groupfile:
             json.dump(groups, groupfile)
+        groupedit(context)
+        break
     compose = '*Send me your thanksgiving / prayer requests.*\n\n(You can update it at any time by sending another message)'
     global users
     for user_id in users:
@@ -122,7 +136,7 @@ def shuffle(update, context):
         randomlist.append(value['name'])
     if len(randomlist) < 2:
         update.message.reply_text(
-            '_Too few people!_', parse_mode=telegram.ParseMode.MARKDOWN)
+            '_More than 1 person needed!_', parse_mode=telegram.ParseMode.MARKDOWN)
         return
     random.shuffle(randomlist)
     compose = '*Prayer Partners*\n\n'
@@ -143,35 +157,45 @@ def shuffle(update, context):
     for group in groups:
         context.bot.send_message(
             chat_id=int(group), text=compose, parse_mode=telegram.ParseMode.MARKDOWN)
+        break
 
 
 def prayer(update, context):
     user_id = str(update.message.chat_id)
     if user_id.startswith('-'):
         return
-    users[user_id]['prayer'] = update.message.text
+    old = users[user_id]['prayer']
+    new = update.message.text
+    try:
+        users[user_id]['prayer'] = re.sub("[<>]", '', new)
+        groupedit(context)
+        update.message.reply_text(
+            '_Saved_', parse_mode=telegram.ParseMode.MARKDOWN)
+    except:
+        users[user_id]['prayer'] = old
+        update.message.reply_text(
+            '_Message could not be updated_', parse_mode=telegram.ParseMode.MARKDOWN)
     with open('users.json', 'w') as userfile:
         json.dump(users, userfile)
-    groupedit(context)
-    update.message.reply_text(
-        '_Saved._', parse_mode=telegram.ParseMode.MARKDOWN)
 
 
 def groupedit(context):
-    compose = '*Prayer List*\n\n'
+    compose = '<b>PRAYER LIST</b>\n\n'
     for value in users.values():
         name = value['name']
         prayer = value['prayer']
-        compose += '*{}*:\n'.format(name)
-        compose += prayer
-        compose += '\n\n'
+        compose += '<u>{}</u>\n'.format(name)
+        if prayer != '':
+            compose += prayer
+            compose += '\n'
+        compose += '\n'
     compose = compose.strip()
     for key, value in groups.items():
         context.bot.edit_message_text(
             chat_id=int(key),
             message_id=int(value),
             text=compose,
-            parse_mode=telegram.ParseMode.MARKDOWN
+            parse_mode=telegram.ParseMode.HTML
         )
 
 
@@ -187,7 +211,7 @@ def main():
 
     loader()
 
-    #updater.start_polling()
+    updater.start_polling()
     updater.start_webhook(listen='0.0.0.0',
                           port=port,
                           url_path=bottoken,
